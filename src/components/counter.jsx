72892e8600449969c585b33d39ef071e62f5ca42
veektrie <102025110+veektrie@users.noreply.github.com>
2022-08-21 08:40:35 +0100

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+  };
+
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
-    let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badges-warning" : "badges-primary";
     return (
       <div>
-        <span className={classes}>{this.state.count}</span>
-        <button className="btn btn-secondar btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm "
+        >
+          {" "}
+          Increment
+        </button>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
