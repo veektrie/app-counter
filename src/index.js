808dcd292df66e4b698898bb44f04317494989bc
veektrie <102025110+veektrie@users.noreply.github.com>
2022-08-22 16:55:30 +0100

    passing data to components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Counters  />
   </React.StrictMode>
 );
 
