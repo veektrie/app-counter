9d4b42331434613feb784de840a335f623beddf6
veektrie <102025110+veektrie@users.noreply.github.com>
2022-08-26 19:59:46 +0100

    state has been updated

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,21 @@ class Counters extends Component {
       { id: 5, value: 0 },
     ],
   };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
