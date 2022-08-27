86775e4256afcccc17d5daaa2c80630477bbad37
veektrie <102025110+veektrie@users.noreply.github.com>
2022-08-27 11:13:44 +0100

    state has been lifted

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {this.props.totalCounters}
+            </span>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
-import Counters from './components/counters';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counters  />
+    <App  />
   </React.StrictMode>
 );
 
