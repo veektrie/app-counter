9073b7cd0fa8cc3bdfafb90c2fc97c0064f45d88
veektrie <102025110+veektrie@users.noreply.github.com>
2022-08-27 12:21:11 +0100

    arguments have been destructured

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </a>
       </div>
